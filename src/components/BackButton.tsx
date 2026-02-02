40bc4a52734915f94d22ace60d32d5c2f5cde1cd
Boris Kamstra <boriskamstra34@gmail.com>
2026-02-02 20:07:54 +0100

    Wat extra ruimte toegevoegd onder de terug knop

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,14 +1,14 @@
-"use client";
-
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 export default function BackButton({ lang }: { lang: string }) {
-    const router = useRouter();
+    // const router = useRouter();
 
     return (
         <Link
-            className="underline-hover"
+            className="underline-hover mb-6 block w-fit"
+            // Tijdelijk gaat de terug knop niet écht terug. Heeft te maken met een cache/navigatie bug in
+            // hoe Next nu met de view transitions omgaat.
             href={`/${lang}`}
             hrefLang={lang}
         >
